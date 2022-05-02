59e83e9bf79fc2f719b9a68e58d700b61c65be67
StormCircuit <tjaccardi98@gmail.com>
2022-05-01 17:09:29 -0700

    Testing from vs code

--- main.java
+++ main.java
@@ -9,6 +9,8 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXMLLoader.load;
 
 public class App extends Application {
     public static void main(String[] args) {
@@ -17,38 +19,6 @@ public class App extends Application {
 
     @Override
     public void init() {
-
-        // setup the stage (creating various objects)
-        Stage primaryStage = new Stage();
-        StackPane primaryStackPane = new StackPane();
-        primaryStage.setTitle("Main App");
-
-        // setup the login input column (labels and input boxes)
-        VBox loginColumn = new VBox();
-        loginColumn.setSpacing(8);
-        loginColumn.setPadding(new Insets(10, 10, 10, 10));
-        loginColumn.getChildren().addAll(
-                new Label("Username:"),
-                new TextField(),
-                new Label("Password:"),
-                new PasswordField(),
-                new Button("Login"));
-        root.getChildren().addAll(loginColumn);
-
-        // setup actions
-        checkLogin.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                // code goes here. Send the input information from username/password box to the
-                // session factory for verification
-            }
-        });
-
-        // setup formatting
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
+        Scene 
     }
 }
