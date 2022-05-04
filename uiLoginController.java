35ec7545ac5c535271dc5d893819d5be06a1eff4
StormCircuit <tjaccardi98@gmail.com>
2022-05-04 02:29:22 -0700

    Some more cleanup

--- uiLoginController.java
+++ uiLoginController.java
@@ -21,7 +21,15 @@ public class uiLoginController {
         //put the actual code to run here.
         //It sounds like we're gonna be using a session factory of sorts so this probably will not be handled by the UI controller
         System.out.println("Success!");
+        String userName = usernameFieldText.getText();
+        String userPassword = passwordFieldText.getText();
+        
+        System.out.println(userName);
+        System.out.println(userPassword);
+        //we would create a new login handler object and send the data here
+        //or whatever library we end up using and however they do it.
 
+        //do login verification/library object creation
     }
 
 }
