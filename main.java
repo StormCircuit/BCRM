6c1cd8f5a4fbd8178f965afeb9c076a52aa77db8
StormCircuit <tjaccardi98@gmail.com>
2022-05-03 21:14:29 -0700

    Merge branch 'main' of https://github.com/StormCircuit/BCRM

--- main.java
+++ main.java
@@ -1,16 +1,18 @@
 import javafx.application.Application;
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
+import javafx.scene.*;
+
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.control.TextField;
+
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.FXMLLoader.load;
 
 // testing!
 public class App extends Application {
@@ -19,7 +21,9 @@ public class App extends Application {
     }
 
     @Override
-    public void init() {
-        Scene 
+    public void start(Stage primaryStage) {
+        //FXMLLoader loader = new FXMLLoader();
+        //loader.setLocation(location);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
     }
 }
