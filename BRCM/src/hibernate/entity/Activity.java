2d83dd8ed7ce27cb15099b69f244ffadf2b4fac4
zrmedina <41082341+zrmedina@users.noreply.github.com>
2022-05-05 19:23:11 -0700

    Add files via upload

---
+++ BRCM/src/hibernate/entity/Activity.java
@@ -0,0 +1,61 @@
+package hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="activity")
+public class Activity {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	@Column(name="activity_id")
+	private int id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="price")
+	private double price;
+
+	public Activity(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Activity [id=" + id + ", name=" + name + ", price=" + price + "]";
+	}
+	
+	
+}
